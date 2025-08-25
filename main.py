4a518b6274ed7fe794cd174446953451b30b2d89
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-08-25 23:49:25 +0300

    2–layer forward pass

--- main.py
+++ main.py
@@ -19,6 +19,20 @@ def sigmoid(z):
 def sigmoid_derivative(z):
     return z * ( 1 - z )
 
+# Compute hidden activations and output
+def forward_pass(X, W1, b1, W2, b2):
+    raw_inputs = [
+        (X[0] * W1[i][0] + X[1] * W1[i][1] + X[2] * W1[i][2] + X[3] * W1[i][3] + b1[i]) 
+        for i in range(len(W1))
+        ]
+
+    hidden_activation = [sigmoid(z) for z in raw_inputs]
+
+    output = hidden_activation[0] * W2[0][0] + hidden_activation[1] * W2[0][1] + b2[0]
+    y_pred = sigmoid(output)
+
+    return y_pred, hidden_activation
+
 two_by_two_vectors = [
     [0, 0, 0, 0],
     [0, 0, 0, 1],
@@ -42,4 +56,5 @@ majority = [
     0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1
 ]
 
-initialize_weights(4, 2, 1)
+W1, b1, W2, b2 = initialize_weights(4, 2, 1)
+forward_pass(two_by_two_vectors[0], W1, b1, W2, b2)
